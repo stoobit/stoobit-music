c21ed3b3624f3c99b0735119d426efbe1c4b5ef4
Till <till.bruegmann@icloud.com>
2025-06-21 19:56:50 +0200

    initialize xcode project

---
+++ stoobit music frontend/stoobit music/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  stoobit music
+//
+//  Created by Till Brügmann on 21.06.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ stoobit music frontend/stoobit music/stoobit_musicApp.swift
@@ -0,0 +1,17 @@
+//
+//  stoobit_musicApp.swift
+//  stoobit music
+//
+//  Created by Till Brügmann on 21.06.25.
+//
+
+import SwiftUI
+
+@main
+struct stoobit_musicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
